245334e1b0553ef8b2501a58e0c6917608b1c1d4
Younes Elberkennou <younes.malek.elberkennou@cern.ch>
2024-08-30 11:48:33 +0200

    streamlining

--- ProtonReco/hello_world.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#head -n 5 $1 >> $2
-echo "Hello, world!"
--- ProtonReco/scripts/run_reco.sh
+++ ProtonReco/scripts/run_reco.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 #cmsRun reco.py inFile=TOTEM21/270000/FABAB63C-8F50-E911-BDCA-0090FAA57730.root outDir=./kjljljlj.root
-cd /afs/cern.ch/user/y/yelberke/scratch1/CMSSW_10_1_7/src/UserCode/ProtonReco/scripts
+
+#cd /afs/cern.ch/user/y/yelberke/scratch1/CMSSW_10_1_7/src/UserCode/ProtonReco/scripts
+
+# 1st argument: input file location
+# 2nd argument: output file location
+# 3rd argument: path to proxy file (see https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookStartingGrid).
+# 4th argument: directory of executable, reco.py.
+
+path="$4/scripts"
+cd $path
+
 mkdir -p $(dirname "$2")
 
 # For file permissions
